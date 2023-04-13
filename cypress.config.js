e9bb568a5a33503e592d7b71c70cd5acd4091b0b
Paul_KrVc <paul1312white@gmail.com>
2023-04-13 21:57:33 +0300

    test API and dashboard for all tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     e2e: {
         projectId: "5c9dpw",
+        projectId: "oza2ny",
         baseUrl: 'http://qamid.tmweb.ru',
         retries :{
             openMode:1,
--- cypress/e2e/spec.api.cy.js
+++ cypress/e2e/spec.api.cy.js
@@ -1,21 +1,68 @@
-it('API test', ()=> {
-
-    cy.request({
-        method: "POST",
-        url: "https://petstore.swagger.io/v2/pet",
-        body: {
-            id: 1,
-            category: {id: 0, name: "string"},
-            name: "bobik",
-            photoUrls: ["string"],
-            tags: [{
-                id: 0,
-                name: "string",
-            }],
-            status: "available",
-        }
-    }).then((response) => {
-        cy.log(JSON.stringify(response.body));
-        expect(response.status).to.eq(200);
-    }); 
+describe('user store API', () => {
+    it.only('create user', () => {
+        cy.request(
+            {
+                url: 'https://petstore.swagger.io/v2/user',
+                body: {
+                    id: 1312,
+                    username: "Petr",
+                    firstName: "Petrovich",
+                    lastName: "Petrov",
+                    email: "petrpetrov@gmail.com",
+                    password: "petryha1312",
+                    phone: "+79850823364",
+                    userStatus: 1
+                },
+                method :'POST' 
+            }
+        ).then((result) => {
+            expect(result.status).is.equal(200);
+        }); 
+    });
+    it.only('get user', () => {
+        cy.request(
+            {
+                url: 'https://petstore.swagger.io/v2/user/Petr',
+                body: {
+                    id: 1312,
+                    username: "Petr",
+                    firstName: "Petrovich",
+                    lastName: "Petrov",
+                    email: "petrpetrov@gmail.com",
+                    password: "petryha1312",
+                    phone: "+79850823364",
+                    userStatus: 1
+                },
+                method:'GET'
+            }
+        ).then((result) =>{
+            expect(result.status).is.equal(200);
+        });
+    });
+    it.only('put user', () => {
+        cy.request({
+            url: 'https://petstore.swagger.io/v2/user/Petr',
+            body: {
+                id: 1312,
+                username: "Ivan",
+                firstName: "Ivanovich",
+                lastName: "Ivanov",
+                email: "petrpetrov@gmail.com",
+                password: "petryha1312",
+                phone: "+79850823364",
+                userStatus: 1
+            },
+            method:'PUT'
+        }).then((result) => {
+            expect(result.status).is.equal(200);
+        });
+    });
+    it.only('delete user', () => {
+        cy.request({
+            url: 'https://petstore.swagger.io/v2/user/Ivan',
+            method:'DELETE'
+        }).then((result) => {
+            expect(result.status).is.equal(200);
+        });
+    });
 });
